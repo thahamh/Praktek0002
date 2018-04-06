b3b79d3a5f63d5031bbcb935b81f336b22d1b634
thahamh <32389972+thahamh@users.noreply.github.com>
2018-04-07 03:53:45 +0800

    Mengisi Nilai Attribute

--- src/praktek0002/ParallelogramAksi.java
+++ src/praktek0002/ParallelogramAksi.java
@@ -2,5 +2,7 @@ package praktek0002;
 public class ParallelogramAksi {
     public static void main(String[] args) {
         Parallelogram r1 = new Parallelogram();
+        r1.alas = 13;
+        r1.tinggi = 5;
     }
 }
