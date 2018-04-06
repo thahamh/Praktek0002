b155c40fda341e2a73913710b36a7b803a3ecb9f
thahamh <32389972+thahamh@users.noreply.github.com>
2018-04-07 03:49:09 +0800

    Membuat Attribute dan Behavior

--- src/praktek0002/Parallelogram.java
+++ src/praktek0002/Parallelogram.java
@@ -1,4 +1,12 @@
 package praktek0002;
 public class Parallelogram {
+    double alas;
+    double tinggi;
     
+    void cetakInfo(){
+        System.out.println("+-----------------+");
+        System.out.println("alas   : "+alas);
+        System.out.println("tinggi : "+tinggi);
+        System.out.println("+-----------------+");
+    }
 }
